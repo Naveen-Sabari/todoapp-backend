af9d02bc83355417a181ab807d10aaaed919fa58
Naveen-Sabari <itznaveen1402@gmail.com>
2024-12-13 15:30:32 +0530

    n

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ import employeeRouters from './Controllers/router.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;  
 
-
-app.use(cors()); 
-
+const corsOptions = {
+    origin: [process.env.CORS_ORIGIN,process.env.CORS_ORIGIN1] ,  
+    methods: ['GET', 'POST', 'DELETE'],  
+    allowedHeaders: ['Content-Type', 'Authorization'],  
+    preflightContinue: false, 
+  };
+app.use(cors());  
 
 app.use(bodyParser.json());
 
 
+app.use('/tasks', employeeRouters);  
+
 app.on('error', (err) => {
     console.error(`Error during startup: ${err.message}`);
 });
-
-
+app.options('*', cors(corsOptions));  
 app.listen(port, () => {
- 
+   
 });
